4cc1c2c830aa053d5065bed1d66c58f3c97e8886
sandeshgowda <sandesh.gowda@gmail.com>
2024-02-25 19:10:07 +0000

    Adding comments for service methods

--- src/main/java/com/property/propertyservice/PropertyService.java
+++ src/main/java/com/property/propertyservice/PropertyService.java
@@ -3,7 +3,8 @@ package com.property.propertyservice;
 import java.util.List;
 
 public interface PropertyService {
+    //This method pulls out all the Property list from database
     List<Property> getAllProperties();
-
+    //This method gets all occupied properties sorted in descending order
     List<Property> getAllOccupiedProperties();
 }
